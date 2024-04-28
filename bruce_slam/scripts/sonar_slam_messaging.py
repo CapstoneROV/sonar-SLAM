4108ed022f4f95aa14535865b4b45d8fa561d740
A.K. Lak <ak7869@nyu.edu>
2024-04-28 22:48:27 +0400

    Modifying MBE stream to fit SLAM requirements

--- bruce_slam/scripts/sonar_slam_messaging.py
+++ bruce_slam/scripts/sonar_slam_messaging.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python
 import rospy
 import numpy as np
+import cv2
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 from bruce_slam.utils.topics import *
 from sonar_oculus.msg import OculusFire, OculusPingUncompressed # importing header messages from the feature extraction node; 
 sonar_pub = rospy.Publisher(SONAR_TOPIC_UNCOMPRESSED, OculusPingUncompressed, queue_size=1)
+debug_pub = rospy.Publisher("debug", Image, queue_size=1)
 
 def callback(data):
+# Convert ROS Image to OpenCV format
+    bridge = CvBridge()
+
+    try:
+        cv_image = bridge.imgmsg_to_cv2(data, desired_encoding="mono8")
+    except CvBridgeError as e:
+        rospy.logerr(e)
+        return
+    
+    # Manipulate the image in OpenCV (flip it horizontally)
+    cv_image = cv2.flip(cv_image, 1)
+
+    # Convert the manipulated image back to a ROS Image message
+    try:
+        ros_image = bridge.cv2_to_imgmsg(cv_image, encoding="mono8")
+    except CvBridgeError as e:
+        rospy.logerr(e)
+        return
+
     new_msg = OculusPingUncompressed()
     new_msg.header = data.header
     new_msg.fire_msg = OculusFire()
@@ -15,18 +37,18 @@ def callback(data):
     new_msg.part_number = 0
     new_msg.start_time = 0 # rospy.Time.now()
 
-    new_msg.bearings = np.linspace(-45, 45, 256).tolist()
-    new_msg.range_resolution = 10.0 / 1024
-    new_msg.num_ranges = 1024
+    new_msg.bearings = np.linspace(-4500, 4500, 256).tolist()
+    new_msg.num_ranges = 512
+    new_msg.range_resolution = 40.0 / new_msg.num_ranges
     new_msg.num_beams = 256
 
-    new_msg.ping = data
+    new_msg.ping = ros_image
     sonar_pub.publish(new_msg)
 
 def listener():
     # Initialize the 'listener' node with a unique name
     rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("/dummy/mbe_sonar_rect", Image, callback)
+    rospy.Subscriber(RECT_IMAGE_TOPIC, Image, callback)
     
     # Keep the node running until it is stopped externally
     rospy.spin()    
